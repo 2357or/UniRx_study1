cf9730287467e5facabe3b4175980dc8660a8bfb
大槻亮太 <ootsukiryouta@ootsukiryoutanoMacBook-Pro.local>
2020-04-19 10:45:50 +0900

    ファイルを追加

---
+++ Test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Test :MonoBehaviour
+{
+    void Start() {
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(_ => Debug.Log("Left!"));
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(1))
+            .Subscribe(_ => Debug.Log("Right!"));
+
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ => Debug.Log("Space!-NO AddTo"));
+
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ => Debug.Log("Space!-AddTo"))
+            .AddTo(this);
+
+        Invoke("destroy", 5.0f);
+    }
+    void destroy() {
+        Debug.Log("destroy");
+        Destroy(this.gameObject);
+    }
+}
